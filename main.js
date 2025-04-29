dd770c783fed5248697a95f3099e36c757715d14
JohanLiebertTheRealOne <gutsbuthalal@gmail.com>
2025-04-29 21:15:08 +0200

    Contine the backend with main.js

--- main.js
+++ main.js
@@ -18,13 +18,25 @@ class Block {
           }, 0);
   }
 
-  afficherInfo() {
-      console.log(`Hash: ${this.hash}`);
-      console.log(`Previous Hash: ${this.previous_hash}`);
-      console.log(`Timestamp: ${this.timestamp}`);
-      console.log(`Propriété 2: ${this.propriete2}`);
+  /**
+   * Retourne un objet contenant les informations du bloc
+   */
+  getInfo() {
+      return {
+          hash: this.hash,
+          previous_hash: this.previous_hash,
+          timestamp: this.timestamp,
+          propriete2: this.propriete2
+      };
   }
 
+  /**
+   * Crée et enregistre un nouveau bloc lié au bloc précédent
+   * @param {Block} previousBlock 
+   * @param {string} propriete2 
+   * @param {Blockchain} blockchain 
+   * @returns {Block}
+   */
   static chainBlocks(previousBlock, propriete2, blockchain) {
       return new Block(previousBlock.hash, propriete2, blockchain);
   }
@@ -39,25 +51,29 @@ class Blockchain {
       this.last_block = this.genesis_block;
   }
 
+  /**
+   * Ajoute un bloc à la chaîne et met à jour last_block
+   * @param {Block} block 
+   */
   addBlock(block) {
       this.block_list.push(block);
       this.last_block = block;
   }
 
-  afficherChain() {
-      this.block_list.forEach((b, idx) => {
-          console.log(`--- Block ${idx} ---`);
-          b.afficherInfo();
-      });
+  /**
+   * Retourne un tableau d'objets contenant les informations de chaque bloc
+   * @returns {Object[]}
+   */
+  getChainInfo() {
+      return this.block_list.map(block => block.getInfo());
   }
 }
 
-// Exemple d'utilisation
+// Exemple d'utilisation :
 const myChain = new Blockchain();
-
-// Ajout de nouveaux blocs
 const bloc1 = new Block(myChain.last_block.hash, 'Premier bloc ajouté', myChain);
 const bloc2 = Block.chainBlocks(myChain.last_block, 'Deuxième bloc ajouté', myChain);
 
-// Affichage de la chaîne complète
-myChain.afficherChain();
+// Récupération des infos sans affichage
+const chainData = myChain.getChainInfo();
+console.log(chainData);
