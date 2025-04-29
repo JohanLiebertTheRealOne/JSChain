0caaee5fa7f283c0d3aa49c9611aeef4a47e262e
JohanLiebertTheRealOne <gutsbuthalal@gmail.com>
2025-04-29 21:06:15 +0200

    BRUH

--- main.js
+++ main.js
@@ -1,34 +1,63 @@
 class Block {
-    // Le constructeur pour initialiser les propriétés
-    constructor(previous_hash = 0, propriete2 = 'Valeur par défaut') {
-      this.previous_hash = previous_hash;  // Previous hash du bloc précédent
-      this.propriete2 = propriete2;         // Propriété personnalisée pour l'exemple
-      this.timestamp = new Date().toISOString();  // Timestamp (date et heure de création du bloc)
-      this.hash = this.calculateHash();      // Hash du bloc (représentation unique)
-    }
-  
-    // Méthode pour calculer le hash du bloc
-    calculateHash() {
-      // On crée un hash simple en combinant les propriétés importantes
-      return `${this.previous_hash}${this.timestamp}${this.propriete2}`.split('').reduce((acc, char) => {
-        acc = (acc << 5) - acc + char.charCodeAt(0);
-        return acc & acc; // pour éviter les dépassements de mémoire
-      }, 0);
-    }
-  
-    // Méthode pour afficher les informations du bloc
-    afficherInfo() {
+  constructor(previous_hash = 0, propriete2 = 'Valeur par défaut', blockchain = null) {
+      this.previous_hash = previous_hash;
+      this.propriete2 = propriete2;
+      this.timestamp = new Date().toISOString();
+      this.hash = this.calculateHash();
+      if (blockchain) {
+          blockchain.addBlock(this);
+      }
+  }
+
+  calculateHash() {
+      return `${this.previous_hash}${this.timestamp}${this.propriete2}`
+          .split('')
+          .reduce((acc, char) => {
+              acc = (acc << 5) - acc + char.charCodeAt(0);
+              return acc & acc;
+          }, 0);
+  }
+
+  afficherInfo() {
       console.log(`Hash: ${this.hash}`);
       console.log(`Previous Hash: ${this.previous_hash}`);
       console.log(`Timestamp: ${this.timestamp}`);
       console.log(`Propriété 2: ${this.propriete2}`);
-    }
-  
-    // Méthode pour lier un bloc au précédent (chaînage)
-    static chainBlocks(previousBlock, currentPropriete2) {
-      return new Block(previousBlock.hash, currentPropriete2);
-    }
   }
-  
-bloc0 = new Block();
-bloc0.afficherInfo();
+
+  static chainBlocks(previousBlock, propriete2, blockchain) {
+      return new Block(previousBlock.hash, propriete2, blockchain);
+  }
+}
+
+class Blockchain {
+  constructor() {
+      this.block_list = [];
+      // Création du bloc genesis
+      new Block(0, 'Genesis Block', this);
+      this.genesis_block = this.block_list[0];
+      this.last_block = this.genesis_block;
+  }
+
+  addBlock(block) {
+      this.block_list.push(block);
+      this.last_block = block;
+  }
+
+  afficherChain() {
+      this.block_list.forEach((b, idx) => {
+          console.log(`--- Block ${idx} ---`);
+          b.afficherInfo();
+      });
+  }
+}
+
+// Exemple d'utilisation
+const myChain = new Blockchain();
+
+// Ajout de nouveaux blocs
+const bloc1 = new Block(myChain.last_block.hash, 'Premier bloc ajouté', myChain);
+const bloc2 = Block.chainBlocks(myChain.last_block, 'Deuxième bloc ajouté', myChain);
+
+// Affichage de la chaîne complète
+myChain.afficherChain();
